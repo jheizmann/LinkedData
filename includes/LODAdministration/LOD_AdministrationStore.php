4abca13fd387b5aa9630eb61e1ae4ac53ca63896
kuehn <kuehn>
2011-05-10 12:24:02 +0000

    fix: 13798 (OntologyBrowser mixes up label and id fields)

--- includes/LODAdministration/LOD_AdministrationStore.php
+++ includes/LODAdministration/LOD_AdministrationStore.php
@@ -442,7 +442,34 @@ QUERY;
 		}
 		return $result;
 	}
+    
+    /**
+     * Every LOD source definition has an ID. This method returns all IDs of
+     * definitions and their labels that are stored in the triple store.
+     *
+     * @return array<id, label>
+     *      An array of all IDs and according labels. If no ID is available, the array is empty.
+     */
+    public function getAllSourceDefinitionIDsAndLabels() {
+        $graph = $this->getDataSourcesGraph();
+        $id    = "smw-lde:ID";
+        $label    = "smw-lde:label";
+        $prefixes = self::getSourceDefinitionPrefixes();
 
+        $query = $prefixes."SELECT ?s ?id ?label FROM <$graph> WHERE { ?s $id  ?id . ?s $label ?label . }";
+
+        $tsa = new LODTripleStoreAccess();
+        $qr = $tsa->queryTripleStore($query, $graph);
+
+        $result = array();
+        if (is_null($qr)) return $result;
+
+        foreach ($qr->getRows() as $row) {
+            $result[] = array($row->getResult("id")->getValue(), $row->getResult("label")->getValue());
+        }
+        return $result;
+    }
+    
 	/**
 	 * Returns the prefixes for all namespaces that are used in triples for
 	 * storing LOD source definitions.
